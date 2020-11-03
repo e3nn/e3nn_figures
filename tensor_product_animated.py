26d92e4e9ca774a38385f0ca53cde4699114e5e2
Mario Geiger <geiger.mario@gmail.com>
2020-11-03 16:00:32 +0100

    update

--- tensor_product_animated.py
+++ tensor_product_animated.py
@@ -33,44 +33,47 @@ def surf(args, x, center):
 
 
 def main(args):
-    if os.path.exists('gif'):
-        shutil.rmtree('gif')
-    os.makedirs('gif')
+    if args.out is None:
+        p = {-1: 'o', 1: 'e'}
+        args.out = f"{args.l1}{p[args.p1]}{args.l2}{p[args.p2]}"
+
+    if os.path.exists(args.out):
+        shutil.rmtree(args.out)
+    os.makedirs(args.out)
+
+    Rs1 = [(1, args.l1, args.p1)]
+    Rs2 = [(1, args.l2, args.p2)]
+    tp = rs.TensorProduct(Rs1, Rs2, o3.selection_rule, normalization='norm')
+    Rs_out = list(rs.split_by_mul(tp.Rs_out))
+
+    x1 = rs.randn(Rs1)
+    x1 = 0.27 * x1 / x1.norm()
+    x2 = rs.randn(Rs2)
+    x2 = 0.27 * x2 / x2.norm()
 
     for i, t in enumerate(tqdm(torch.linspace(0, 1, args.steps + 1)[:-1])):
-        c1 = math.cos(2 * math.pi * t)
-        s1 = math.sin(2 * math.pi * t)
-        c2 = math.cos(4 * math.pi * t)
-        s2 = math.sin(4 * math.pi * t)
-
-        x1 = torch.tensor([0.1, 0.05, 0.2])
-        x1 = 0.27 * x1 / x1.norm()
-
-        if args.animation == "change":
-            x2 = c1 * torch.tensor([0.0, 0.0, 0.25, 0.0, 0.0])
-            x2 += s1 * torch.tensor([0.25, 0.0, 0.0, 0., 0.0])
-            x2 += s2 * torch.tensor([0.0, 0.25, 0.0, 0., 0.0])
-            x2 += c2 * torch.tensor([0.0, 0., 0.0, 0., 0.25])
-            x2 = 0.27 * x2 / x2.norm()
 
         if args.animation == "rotation":
-            x2 = torch.tensor([0.0, 0.0, 0.25, 0.0, 0.0])
-            a = 2 * math.pi * t
-            b = 2 * math.pi * t
-            c = 2 * math.pi * t
-            x1 = rs.rep(1, a, b, c) @ x1
-            x2 = rs.rep(2, a, b, c) @ x2
+            a = 4 * math.pi * t
+            b = 0
+            c = 0
+            p = 0 if t < 0.5 else 1
+            gx1 = rs.rep(Rs1, a, b, c, p) @ x1
+            gx2 = rs.rep(Rs2, a, b, c, p) @ x2
+        if args.animation == "random":
+            gx1 = rs.randn(Rs1)
+            gx2 = rs.randn(Rs2)
 
-        tp = rs.TensorProduct(1, 2, o3.selection_rule, normalization='norm')
-        x3 = 4. * tp(x1, x2)
-        out1, out2, out3 = rs.cut(x3, [1], [2], [3])
-
+        xleft = -0.5 * (len(Rs_out) + 2 - 1)
         data = [
-            surf(args, x1, torch.tensor([-2, 0, 0.0])),
-            surf(args, x2, torch.tensor([-1, 0, 0.0])),
-            surf(args, out1, torch.tensor([0, 0, 0.0])),
-            surf(args, out2, torch.tensor([1, 0, 0.0])),
-            surf(args, out3, torch.tensor([2, 0, 0.0])),
+            surf(args, gx1, torch.tensor([xleft, 0, 0.0])),
+            surf(args, gx2, torch.tensor([xleft + 1, 0, 0.0])),
+        ]
+
+        outs = rs.cut(4. * tp(gx1, gx2), *Rs_out)
+        data += [
+            surf(args, out, torch.tensor([xleft + 2 + i, 0, 0.0]))
+            for i, out in enumerate(outs)
         ]
 
         axis = dict(
@@ -83,12 +86,12 @@ def main(args):
         )
 
         layout = dict(
-            width=5 * args.height,
+            width=(2 + len(Rs_out)) * args.height,
             height=args.height,
             scene=dict(
                 xaxis=dict(
                     **axis,
-                    range=[-2.5, 2.5]
+                    range=[xleft - 0.5, -xleft + 0.5]
                 ),
                 yaxis=dict(
                     **axis,
@@ -99,51 +102,13 @@ def main(args):
                     range=[-0.5, 0.5]
                 ),
                 aspectmode='manual',
-                aspectratio=dict(x=10, y=2, z=2),
+                aspectratio=dict(x=2 * (2 + len(Rs_out)), y=2, z=2),
                 camera=dict(
                     up=dict(x=0, y=0, z=1),
                     center=dict(x=0, y=0, z=0),
-                    eye=dict(x=0, y=-2, z=0),
+                    eye=dict(x=0, y=-10, z=0),
                     projection=dict(type='orthographic'),
                 ),
-                annotations=[
-                    dict(
-                        showarrow=False,
-                        x=-1.5,
-                        y=0,
-                        z=0,
-                        text=r"$\Huge\otimes$",
-                        xanchor="center",
-                        font=dict(color=args.color_text)
-                    ),
-                    dict(
-                        showarrow=False,
-                        x=-0.5,
-                        y=0,
-                        z=0,
-                        text=r"$\Huge=$",
-                        xanchor="center",
-                        font=dict(color=args.color_text)
-                    ),
-                    dict(
-                        showarrow=False,
-                        x=0.5,
-                        y=0,
-                        z=0,
-                        text=r"$\Huge\oplus$",
-                        xanchor="center",
-                        font=dict(color=args.color_text)
-                    ),
-                    dict(
-                        showarrow=False,
-                        x=1.5,
-                        y=0,
-                        z=0,
-                        text=r"$\Huge\oplus$",
-                        xanchor="center",
-                        font=dict(color=args.color_text)
-                    ),
-                ]
             ),
             paper_bgcolor=args.color_bg,
             plot_bgcolor=args.color_bg,
@@ -151,9 +116,9 @@ def main(args):
         )
 
         fig = go.Figure(data=data, layout=layout)
-        fig.write_image('gif/{:03d}.png'.format(i))
+        fig.write_image('{}/{:03d}.png'.format(args.out, i))
 
-    subprocess.check_output(["convert", "-delay", "3", "-loop", "0", "-dispose", "2", "gif/*.png", "output.gif"])
+    subprocess.check_output(["convert", "-delay", "5", "-loop", "0", "-dispose", "2", "{}/*.png".format(args.out), "{}.gif".format(args.out)])
 
 
 if __name__ == '__main__':
@@ -161,10 +126,15 @@ if __name__ == '__main__':
 
     parser.add_argument("--height", type=int, default=200)
     parser.add_argument("--res", type=int, default=100)
-    parser.add_argument("--steps", type=int, default=30)
+    parser.add_argument("--steps", type=int, default=90)
     parser.add_argument("--color_bg", type=str, default="rgba(0,0,0,0)")
     parser.add_argument("--color_text", type=str, default="rgb(255,255,255)")
-    parser.add_argument("--cmap", type=str, default="bwr")
+    parser.add_argument("--cmap", type=str, default="plasma")
     parser.add_argument("--animation", type=str, default="rotation")
+    parser.add_argument("--out", type=str)
+    parser.add_argument("--l1", type=int, default=1)
+    parser.add_argument("--l2", type=int, default=1)
+    parser.add_argument("--p1", type=int, default=-1)
+    parser.add_argument("--p2", type=int, default=-1)
 
     main(parser.parse_args())
