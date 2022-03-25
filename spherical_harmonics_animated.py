9f60aa7e60b65bceda10ac1d72f4cdf36f6515f6
Mario Geiger <geiger.mario@gmail.com>
2022-03-25 11:20:46 -0400

    spherical harmonics

--- spherical_harmonics_animated.py
+++ spherical_harmonics_animated.py
@@ -8,6 +8,7 @@ import plotly.graph_objs as go
 import torch
 from tqdm.auto import tqdm
 from e3nn.io import SphericalTensor
+from e3nn import o3
 
 
 def get_cmap(x):
@@ -17,38 +18,38 @@ def get_cmap(x):
         return [[0, '#9F1A9B'], [0.25, '#0D1286'], [0.5, '#000000'], [0.75, '#F58C45'], [1, '#F0F524']]
 
 
-def surf(args, x, center):
-    st = SphericalTensor(args.L, p_val=1, p_arg=-1)
-
-    return [
-        go.Surface(
-            **d,
-            showscale=False,
-            cmin=-0.33,
-            cmax=0.33,
-            colorscale=get_cmap(args.cmap),
-        )
-        for d in st.plotly_surface(x, res=args.res, centers=center, normalization='component')
-    ]
-
-
 def main(args):
     if os.path.exists(args.out):
         shutil.rmtree(args.out)
     os.makedirs(args.out)
 
-    xs = torch.randn(args.pitchs, (args.L + 1)**2)
-    print(xs)
+    x0 = torch.eye((args.L + 1)**2)
+    st = SphericalTensor(args.L, p_val=1, p_arg=-1)
 
-    for i, t in enumerate(tqdm(torch.linspace(0, 1, args.steps + 1)[:-1])):
-        t = t.item()
-        j = round(t // (1 / args.pitchs))
-        t = (t * args.pitchs) % 1
-        x0 = xs[j]
-        x1 = xs[j + 1] if j + 1 < args.pitchs else xs[0]
-        x = x0 * (1 - t) + x1 * t
+    centers = torch.tensor([
+        [l - max(0, m), 0, l + min(0, m)]
+        for l in range(args.L + 1)
+        for m in range(-l, l + 1)
+    ])
 
-        data = surf(args, x, torch.tensor([0, 0, 0.0]))
+    centers = centers - torch.tensor([args.L / 2, 0, args.L / 2])
+    centers = 2 * centers
+    centers = centers * torch.tensor([1.0, 0.0, -1.0])
+
+    for i, angle in enumerate(tqdm(torch.linspace(0, 2 * torch.pi, args.steps + 1)[:-1])):
+        D = st.D_from_angles(*o3.axis_angle_to_angles(torch.tensor([1.0, 0.0, 1.0]), angle))
+        x = torch.einsum("ij,zi->zj", x0, D)
+
+        data = [
+            go.Surface(
+                **d,
+                showscale=False,
+                cmin=-0.33,
+                cmax=0.33,
+                colorscale=get_cmap(args.cmap),
+            )
+            for d in st.plotly_surface(x, res=args.res, centers=centers, normalization='norm')
+        ]
 
         axis = dict(
             showbackground=False,
@@ -57,7 +58,7 @@ def main(args):
             zeroline=False,
             title='',
             nticks=3,
-            range=[-3, 3]
+            range=[-args.L - 1.5, args.L + 1.5],
         )
 
         layout = dict(
@@ -74,9 +75,9 @@ def main(args):
                     **axis,
                 ),
                 aspectmode='manual',
-                aspectratio=dict(x=4, y=4, z=4),
+                aspectratio=dict(x=2, y=2, z=2),
                 camera=dict(
-                    up=dict(x=0, y=0, z=1),
+                    up=dict(x=0, y=0, z=-1),
                     center=dict(x=0, y=0, z=0),
                     eye=dict(x=0, y=-5, z=0),
                     projection=dict(type='orthographic'),
@@ -99,10 +100,9 @@ if __name__ == '__main__':
     parser.add_argument("--height", type=int, default=300)
     parser.add_argument("--res", type=int, default=100)
     parser.add_argument("--steps", type=int, default=60)
-    parser.add_argument("--pitchs", type=int, default=4)
     parser.add_argument("--color_bg", type=str, default="rgba(0,0,0,0)")
     parser.add_argument("--color_text", type=str, default="rgb(255,255,255)")
-    parser.add_argument("--cmap", type=str, default="plasma")
+    parser.add_argument("--cmap", type=str, default="bwr")
     parser.add_argument("--L", type=int, default=2)
     parser.add_argument("--out", type=str, default="gif")
 
